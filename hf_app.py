54a4f0708adea5b93f34b8692b0ed195636259eb
prathamesh-ai <prathamesh84.m@gmail.com>
2024-10-12 16:13:57 +0530

    Update hf_app.py

--- hf_app.py
+++ hf_app.py
@@ -19,7 +19,7 @@ with st.sidebar:
 
 generic_url=st.text_input("URL",label_visibility="collapsed")
 
-llm =ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
+llm =ChatGroq(model="llama-3.1-8b-instant", groq_api_key=groq_api_key)
 
 prompt_template="""
 Provide a summary of the following content in 300 words:
