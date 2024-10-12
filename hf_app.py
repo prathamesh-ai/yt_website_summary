dc7c65c9458914ec7eda0bd1cf41c7bfedf551d7
prathamesh-ai <prathamesh84.m@gmail.com>
2024-10-12 15:52:54 +0530

    Update hf_app.py

--- hf_app.py
+++ hf_app.py
@@ -40,7 +40,7 @@ if st.button("Summarize"):
             with st.spinner("Waiting..."):
                 ## loading the website or yt video data
                 if "youtube.com" in generic_url:
-                    loader=YoutubeLoader.from_youtube_url(generic_url,add_video_info=True)
+                    loader=YoutubeLoader.from_youtube_url(generic_url.split('&')[0],add_video_info=True)
                 else:
                     loader=UnstructuredURLLoader(urls=[generic_url],ssl_verify=False,
                                                  headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
